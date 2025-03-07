27b00d2635064f9338c4f6fd1c5627b8d3a67399
Hussein El Amouri <alamourihusein@gmail.com>
2025-03-07 14:32:27 +0100

    updated gitignore

--- activate_EEGPT_env.sh
+++ activate_EEGPT_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /mnt/d/python/envs/EEGPT_env/bin/activate #H1N1desktop
-# source /home/hussein/WorSpace/LBW/EEGPT_venv/bin/activate #hussein@mia-ps-port028
+# source /mnt/d/python/envs/EEGPT_env/bin/activate #H1N1desktop
+source /home/hussein/WorSpace/LBW/EEGPT_venv/bin/activate #hussein@mia-ps-port028
